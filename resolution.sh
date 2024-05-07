add5624d6cd543cd04142cba73094b49e083703a
Thadeu Tucci <thadeutucci@gmail.com>
2024-05-07 17:37:30 +0200

    update resolution

--- resolution.sh
+++ resolution.sh
@@ -1,3 +1,3 @@
-xrandr --output eDP-1 --primary --mode 2560x1600 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
 
 feh --bg-fill ~/dotfiles/Backgrounds/switzerland_alps_mountains_night_beautiful_landscape_99817_3840x2400.jpg
